65ec13c8871448f2f87d20b3c663dfa662277a5e
Рассказов Александр <alexrassk@outlook.com>
2016-10-23 19:46:26 +0700

    Лекция 5 слайд 9

---
+++ Clients/Clients/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    abstract class Client
+    {
+        protected int Id;
+        protected int MainPhone;
+        protected int OrderPrice;
+        public string ClientInfo;
+    }
+}
---
+++ Clients/Clients/LLC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clients
+{
+    class LLC : Client
+    {
+        private string Name;
+        private int BankAccount;
+
+        public new string ClientInfo => $"ООО {Name} имеет сумму заказа равную {OrderPrice}";
+
+        public LLC(int id, int mainPhone, int orderPrice, string name, int bankAccount)
+        {
+            Id = id;
+            MainPhone = mainPhone;
+            OrderPrice = orderPrice;
+            Name = name;
+            BankAccount = bankAccount;
+        }
+    }
+}
