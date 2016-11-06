facc4b6b75a67029b393421651c750eb7d8c098e
Рассказов Александр <alexrassk@outlook.com>
2016-11-06 15:51:05 +0700

    Задача из лекции 7, слайд 12

---
+++ SimpleFraction/SimpleFraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SimpleFraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfSimpleFractions = new List<SimpleFraction>();
+
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            for (var i = 0; i < 10; i++)
+            {
+                listOfSimpleFractions.Add(new SimpleFraction(rnd.Next(), rnd.Next()));
+            }
+
+            listOfSimpleFractions.Sort();
+            
+            Console.WriteLine(SumOfAllFractions(listOfSimpleFractions));
+            Console.ReadKey();
+        }
+
+        private static SimpleFraction SumOfAllFractions(IReadOnlyList<SimpleFraction> listOfSimpleFractions)
+        {
+            var sumOfAllFractions = listOfSimpleFractions[0];
+
+            for (var i = 1; i < listOfSimpleFractions.Count; i++)
+            {
+                sumOfAllFractions += listOfSimpleFractions[i];
+            }
+
+            return sumOfAllFractions;
+        }
+    }
+}
---
+++ SimpleFraction/SimpleFraction/SimpleFraction.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFraction
+{
+    public struct SimpleFraction : IComparable
+    {
+        public long Numerator { get; private set; }
+        public long Denominator { get; private set; }
+
+        public SimpleFraction(long x, long y)
+        {
+            if (y == 0)
+            {
+                throw new Exception("The denominator cannot be zero");
+            }
+
+            Numerator = x;
+            Denominator = y;
+
+            ReduceFraction();
+        }
+
+        private long GCD(long x, long y)
+        {
+            return y == 0 ? x : GCD(y, x % y);
+        }
+
+        private void ReduceFraction()
+        {
+            var gcd = GCD(Numerator, Denominator);
+            Numerator /= gcd;
+            Denominator /= gcd;
+        }
+
+        public override string ToString()
+        {
+            var sign = (double)Numerator/Denominator > 0 ? "" : "-";
+            return $"{sign}{Math.Abs(Numerator)}/{Math.Abs(Denominator)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            var fraction = (SimpleFraction) obj;
+
+            return (Numerator * fraction.Denominator) > (fraction.Numerator * Denominator) ? 1 : (Numerator * fraction.Denominator) < (fraction.Numerator * Denominator) ? -1 : 0;
+        }
+
+        public static SimpleFraction operator +(SimpleFraction f1, SimpleFraction f2)
+        {
+            return new SimpleFraction(f1.Numerator * f2.Denominator + f2.Numerator * f1.Denominator, f1.Denominator * f2.Denominator);
+        }
+
+        public static SimpleFraction operator -(SimpleFraction f1, SimpleFraction f2)
+        {
+            return new SimpleFraction(f1.Numerator * f2.Denominator - f2.Numerator * f1.Denominator, f1.Denominator * f2.Denominator);
+        }
+
+        public static SimpleFraction operator *(SimpleFraction f1, SimpleFraction f2)
+        {
+            return new SimpleFraction(f1.Numerator * f2.Numerator, f1.Denominator * f2.Denominator);
+        }
+
+        public static SimpleFraction operator /(SimpleFraction f1, SimpleFraction f2)
+        {
+            return new SimpleFraction(f1.Numerator * f2.Denominator, f1.Denominator * f2.Numerator);
+        }
+    }
+}
